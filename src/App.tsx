cbcad37b930ab18e38fecedbe6b73bc3f3bd9c7b
altrios <famartinez.ti@gmail.com>
2026-06-22 12:47:50 +0200

    propiedades

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/form.component";
 
 function App() {
+  const [nombre, setNombre] = useState("Fernando")
+  const normal="variable"
+  const random= Math.floor(Math.random()*100)
+
   return (
     <>
-      <Form />
-      <Form></Form>
+    <h1>{nombre}</h1>
+
+    <p>numero: {random}</p>
+      <Form nombramos={nombre} setterName={setNombre} />
+      {/* <Form>
+
+      </Form> */}
     </>
   );
 }
--- src/components/form.component.tsx
+++ src/components/form.component.tsx
@@ -1,7 +1,34 @@
-export default function Form() {
+import { useState } from "react";
+import "./form.css";
+
+interface hijo {
+  nombramos: string;
+  setterName: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Form({ nombramos, setterName }: hijo) {
+
+    const [hijo, setHijo]=useState("hijo")
+  const random = Math.floor(Math.random() * 100);
   return (
     <>
-      <h1>Formulario</h1>
+      <div className="formulario">
+        <p>{random}</p>
+        <h1 className="titulo">Formulario</h1>
+        <h2>el nombre del hijo es: {nombramos}</h2>
+
+        <button
+          onClick={() => {
+            setterName(nombramos + "a");
+          }}
+        >
+          Agregar A
+        </button>
+
+
+        <h2>{hijo}</h2>
+        <button onClick={()=>{setHijo(hijo+"B")}}>AgregarB</button>
+      </div>
     </>
   );
 }

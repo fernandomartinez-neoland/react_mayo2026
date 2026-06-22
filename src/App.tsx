19d00ae3eb6c601cb6bcfa407fcc1be7504e165b
altrios <famartinez.ti@gmail.com>
2026-06-22 10:33:26 +0200

    formulario

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/form.component";
+
+function App() {
+  return (
+    <>
+      <Form />
+      <Form></Form>
+    </>
+  );
+}
+
+export default App;
